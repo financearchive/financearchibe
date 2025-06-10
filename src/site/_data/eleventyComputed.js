3daac5db9717e4266e686a8d38c03704ff3b8797
금융아카이브 <85724325+financearchive@users.noreply.github.com>
2025-06-11 00:58:15 +0900

    Update eleventyComputed.js

--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -1,9 +1,14 @@
-const { getGraph } = require("../../helpers/linkUtils");
-const { getFileTree } = require("../../helpers/filetreeUtils");
-const { userComputed } = require("../../helpers/userUtils");
-
 module.exports = {
-  graph: (data) => getGraph(data),
-  filetree: (data) => getFileTree(data),
-  userComputed: (data) => userComputed(data)
+  eleventyComputed: {
+    graph: async function() {
+      const nodes = {};
+      const templates = this.templates; // 또는 eleventy.getTemplates()와 같은 방법으로 템플릿 가져오기
+      for (const template of templates) {
+        const content = await template.read();
+        // 내용 처리 (예: 노드 생성 등)
+      }
+      // 계산된 그래프 반환
+      return { graph: nodes };
+    }
+  }
 };
